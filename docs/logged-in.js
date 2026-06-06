7912765b4f65dc1893cf8e9239a72aad3d4459b4
Magooster1000 <145853867+mag10000@users.noreply.github.com>
2026-06-06 07:02:30 -0700

    Update logged-in.js

--- docs/logged-in.js
+++ docs/logged-in.js
@@ -1,2 +1,2 @@
 console.log("You are logged in, your session token is:")
-console.log(Document.URL.replace("https://mag10000.github.io/magooster1000-s-purple/logged-in?id=",""))
+console.log(document.URL.replace("https://mag10000.github.io/magooster1000-s-purple/logged-in?id=",""))
