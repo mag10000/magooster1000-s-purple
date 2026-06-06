5068874b05df4da28199ed676ba3108f20a5ccc8
Magooster1000 <145853867+mag10000@users.noreply.github.com>
2026-06-06 06:59:43 -0700

    Update logged-in.js

--- docs/logged-in.js
+++ docs/logged-in.js
@@ -1,3 +1,2 @@
 console.log("You are logged in, your session token is:")
-const searchParams = new URLSearchParams(document.URL);
-console.log(searchParams)
+console.log(Document.URL.replace("https://mag10000.github.io/magooster1000-s-purple/logged-in?id=",""))
